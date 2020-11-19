d6c19866d5abd94798927ead8925f7f654d0fc6e
Shaleen Jain <shaleen@jain.sh>
2020-11-19 17:13:26 +0530

    Revert "remove live demo button"
    This reverts commit 2e720c4c85b716f696d91ea508f2266989e66ccb.

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,15 @@ export default function IndexPage() {
                     Join Waitlist
                   </a>
                 </div>
+                <div class="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="https://demo.unipeer.exchange"
+                    target="_blank"
+                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                  >
+                    Live demo
+                  </a>
+                </div>
               </div>
             </div>
           </main>
