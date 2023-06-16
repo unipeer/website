9b6ca30c49f4f388abf3cc31ce752cc216af5861
Ayush Iyankan <75990868+AyushIyankan@users.noreply.github.com>
2023-06-16 13:44:42 +0530

    several ui improvements (#14)
    * minor fixes in navbar
    
    * several fixes and changes in landing page (not yet fully responsive), adapt new design changes to build
    
    * complete responsiveness of landing page
    
    * fix fonts in /terms
    
    * fix fonts in /privacy
    
    * fix fonts in /aml_kyc_policy
    
    * minor button fix in Subscribe.js
    
    * several ui improvements

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -6,17 +6,18 @@ export default function Subscribe() {
       className="relative max-w-[924px] my-16  md:mb-20 mx-auto "
       id="features"
     >
-      <div className="bg-primary-100 flex flex-col-reverse justify-center items-center md:flex-row rounded-2xl p-4 sm:p-8 md:p-12 ">
-        <div className="w-8/12 flex flex-col justify-center items-center md:justify-start  md:items-start ">
-          <h2 className="font-Jost font-bold text-dark-900 text-2xl hidden md:flex">
-            Subscribe for updates from us
+      <div className="bg-primary-100 mx-5 flex flex-col-reverse justify-center items-center md:flex-row rounded-2xl p-4 sm:p-8 md:p-12 ">
+        <div className="w-full flex flex-col justify-center items-center md:justify-start  md:items-start sm:w-8/12">
+          <h2 className="font-Jost font-bold text-dark-900 text-2xl flex">
+            Subscribe for updates{" "}
+            <span className="hidden md:flex">&nbsp;from us</span>
           </h2>
           {/* <h2 className=" font-Jost text-[#020423] w-full text-xl md:text-xl md:hidden block">
             Subscribe for updates
           </h2> */}
           <form
             method="POST"
-            className="py-4 flex flex-col justify-center items-center md:justify-start md:items-start"
+            className="py-4 w-full flex flex-col justify-center items-center md:justify-start md:items-start "
             name="newsletter"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
@@ -28,7 +29,7 @@ export default function Subscribe() {
               name="email"
               placeholder="Your email address"
               required={true}
-              className="font-Inter text-dark-200  block md:w-[25rem] px-6 md:px-4 py-3 text-base appearance-none border border-dark-100  bg-white rounded-md placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
+              className="font-Inter text-dark-200  block w-full md:w-[25rem] px-6 md:px-4 py-3 text-base appearance-none border border-dark-100  bg-white rounded-md placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
               style={{
                 boxShadow: "0 6px 15px 2px rgba(0, 0, 0, 0.06)",
               }}
@@ -36,14 +37,18 @@ export default function Subscribe() {
 
             <button
               type="submit"
-              className="mt-4 md:mt-12 font-Inter md:text-4 px-4 md:px-6 py-3 md:py-4 font-semibold text-white rounded-md bg-primary-500 hover:bg-primary-700 transition ease-in-out duration-150"
+              className="mt-4 md:mt-12 font-Inter md:text-4 px-4 md:px-6 py-3 md:py-4 font-semibold text-white rounded-md bg-primary-500 hover:bg-primary-700 transition ease-in-out duration-150 w-full md:w-auto"
             >
               Send me updates
             </button>
           </form>
         </div>
         <div className="w-4/12 flex flex-row justify-center items-center">
-          <img className="md:w-40" src="assets/images/Subscribe.svg" alt="" />
+          <img
+            className="md:w-[145px]"
+            src="assets/images/Subscribe.svg"
+            alt=""
+          />
         </div>
       </div>
     </div>
